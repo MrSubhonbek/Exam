a22aeca9d7285d9ee4a141c5ed074e02e208f495
Subhonbek <basubhonbek@gmail.com>
2022-07-04 13:15:20 +0300

    validate data in setting

--- src/App.tsx
+++ src/App.tsx
@@ -2,37 +2,57 @@ import { ChangeEvent, useState } from 'react'
 import Bottom from './components/buttons/Buttons'
 import { Setting } from './components/setting/Setting'
 import Windows from './components/window/Windows'
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import s from './styles/App.module.css'
 export default function App() {
     const [count, setCount] = useState<number>(0)
     const [start, setStart] = useState<number>(0);
     const [end, setEnd] = useState<number>(5);
     const [step, setStep] = useState<number>(1);
+    const [errorEnter, setErrorEnter] = useState(false)
 
-    const onChangeStartHandler = (event: ChangeEvent<HTMLInputElement>) => setStart(Number(event.currentTarget.value))
-    const onChangeEndHandler = (event: ChangeEvent<HTMLInputElement>) => setEnd(Number(event.currentTarget.value))
+    const onChangeStartHandler = (event: ChangeEvent<HTMLInputElement>) => {
+        setStart(Number(event.currentTarget.value));
+        validDataStart(Number(event.currentTarget.value));
+    }
+    const onChangeEndHandler = (event: ChangeEvent<HTMLInputElement>) => {
+        setEnd(Number(event.currentTarget.value));
+        validDataEnd(Number(event.currentTarget.value));
+    }
     const onChangeStepHandler = (event: ChangeEvent<HTMLInputElement>) => setStep(Number(event.currentTarget.value))
 
+    function validDataStart(count: number) {
+        (count >= end || count < 0)
+            ? setErrorEnter(true)
+            : setErrorEnter(false);
+    }
+    function validDataEnd(count: number) {
+        (count <= start || count < 0)
+            ? setErrorEnter(true)
+            : setErrorEnter(false);
+    }
 
     return (
         <div className={s.wrapper}>
             <div className={s.app}>
                 <Routes>
                     <Route path="/" element={<>
-                        <Windows count={count} end={end}/>
-                        <Bottom setCount={setCount} count={count} end={end} start = {start} step={step} />
+                        <Windows count={count} end={end} />
+                        <Bottom setCount={setCount} count={count} end={end} start={start} step={step} />
                     </>} />
                     <Route path="/setting"
                         element={
                             <Setting
                                 end={end}
+                                errorEnter={errorEnter}
                                 start={start}
                                 step={step}
+                                setCount = {setCount}
                                 onChangeEndHandler={onChangeEndHandler}
                                 onChangeStartHandler={onChangeStartHandler}
                                 onChangeStepHandler={onChangeStepHandler}
-                            />}
+                            />
+                        }
                     />
                 </Routes>
             </div>
--- src/components/setting/Setting.tsx
+++ src/components/setting/Setting.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, ChangeEventHandler, useState } from 'react';
+import { ChangeEvent, Dispatch, SetStateAction } from 'react';
 import { useNavigate } from 'react-router-dom';
 import UniversalButton from '../buttons/UniversalButton'
 import s from './Setting.module.css'
@@ -6,30 +6,39 @@ interface IProps {
   start: number
   end: number
   step: number
-  onChangeStartHandler: (event: ChangeEvent<HTMLInputElement>)=>void
-  onChangeEndHandler: (event: ChangeEvent<HTMLInputElement>)=>void
-  onChangeStepHandler: (event: ChangeEvent<HTMLInputElement>)=>void
+  errorEnter: boolean
+  setCount: Dispatch<SetStateAction<number>>
+  onChangeStartHandler: (event: ChangeEvent<HTMLInputElement>) => void
+  onChangeEndHandler: (event: ChangeEvent<HTMLInputElement>) => void
+  onChangeStepHandler: (event: ChangeEvent<HTMLInputElement>) => void
 }
-export function Setting(props:IProps) {
+export function Setting(props: IProps) {
   const navigate = useNavigate();
-  let errorEnter: boolean = false;
- const routerHandler = () => navigate('/');
-  
+  const routerHandler = () => {
+    props.setCount(props.start)
+    navigate('/')
+  };
+
   return (
     <div className={s.setting}>
       <p>Setting</p>
       <div className={s.wrapper}>
         <div className={s.formField}>
           <input value={props.start} className={s.input} onChange={props.onChangeStartHandler} type="number" name="start" placeholder='Start' />
+          <p className={s.text}>Start</p>
         </div>
         <div className={s.formField}>
           <input value={props.end} className={s.input} onChange={props.onChangeEndHandler} type="number" name="end" placeholder='End' />
+          <p className={s.text}>End</p>
         </div>
         <div className={s.formField}>
           <input value={props.step} className={s.input} onChange={props.onChangeStepHandler} type="number" name="step" placeholder='Step' />
+          <p className={s.text}>Step</p>
         </div>
       </div>
+
       <UniversalButton title='Save Setting' onClickHandler={routerHandler} style={s.setSetting} />
+      {props.errorEnter && <p className={s.error}>Incorrect data!</p>}
     </div>
   )
 }
